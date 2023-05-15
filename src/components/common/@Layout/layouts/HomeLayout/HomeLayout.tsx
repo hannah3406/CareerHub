e7e77782a3004a9deb0a0be24d8a4de159c66352
hajungkim <hajung3406@naver.com>
2023-05-15 10:50:49 +0900

    통계관리 가리기, 모바일 가리기

--- src/components/common/@Layout/layouts/HomeLayout/HomeLayout.tsx
+++ src/components/common/@Layout/layouts/HomeLayout/HomeLayout.tsx
@@ -1,4 +1,4 @@
-import { ContainerProps, Grid } from "@chakra-ui/react";
+import { Box, ContainerProps, Flex, Grid } from "@chakra-ui/react";
 import HomeFooter from "../../footer";
 
 import HomeHeader from "../../headers/HomeHeader";
@@ -20,29 +20,41 @@ const HomeLayout = ({
   isHideHeader,
 }: HomeLayoutProps) => {
   return (
-    <Grid
-      minH="100vh"
-      h="100%"
-      // maxW="1200px"
-      m="0 auto"
-      gridTemplateRows="auto 1fr auto"
-      {...containerProps}
-    >
-      {!isHideHeader && (
-        <header
-          style={{
-            position: "sticky",
-            top: 0,
-            zIndex: 99,
-            backgroundColor: "#fff",
-          }}
-        >
-          {header}
-        </header>
-      )}
-      <main>{content}</main>
-      <footer>{footer}</footer>
-    </Grid>
+    <>
+      <Grid
+        minH="100vh"
+        h="100%"
+        m="0 auto"
+        gridTemplateRows="auto 1fr auto"
+        {...containerProps}
+        display={{ base: "none", sm: "grid" }}
+      >
+        {!isHideHeader && (
+          <header
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 99,
+              backgroundColor: "#fff",
+            }}
+          >
+            {header}
+          </header>
+        )}
+        <main>{content}</main>
+        <footer>{footer}</footer>
+      </Grid>
+      <Flex
+        minH="100vh"
+        h="100%"
+        w="100%"
+        justifyContent="center"
+        alignItems="center"
+        display={{ base: "flex", sm: "none" }}
+      >
+        모바일 작업중입니다!! PC 로 접속해주세요
+      </Flex>
+    </>
   );
 };
 
