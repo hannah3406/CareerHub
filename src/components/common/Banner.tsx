b087c42bbc04213fad04b2fc74903cd6d2724692
hajungkim <hajung3406@naver.com>
2023-05-24 11:17:02 +0900

    배너 이미지 적용

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,39 +1,76 @@
-// Fleximport Slider from "react-slick";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styled from "@emotion/styled";
 
 import { Flex } from "@chakra-ui/react";
 const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    pauseOnHover: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    fade: true,
+  };
   return (
-    <Flex p="0 30px" m="0 auto" w="1000px">
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        borderRadius="50px"
-        w="100%"
-      >
-        <img
-          src={
-            process.env.PUBLIC_URL + `/assets/image/banner/banner-main-1.png`
-          }
-          alt={`배너이미지}`}
-          style={{
-            width: "100%",
-            borderRadius: "20px",
-          }}
-        />
-      </Flex>
-      {/* {[1, 2].map((el) => (
-        <Flex key={el} justifyContent="center" alignItems="center">
-          <img
-            src={
-              process.env.PUBLIC_URL + `/assets/image/banner/banner-${el}.png`
-            }
-            alt={`배너이미지-${el}}`}
-          />
-        </Flex>
-      ))} */}
-    </Flex>
+    <SliderWrapper>
+      <Slider {...settings}>
+        {[1, 2, 3].map((el) => (
+          <Flex
+            key={el}
+            justifyContent="center"
+            alignItems="center"
+            m="0 auto"
+            boxShadow="md"
+          >
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/assets/image/banner/main-banner${el}.png`
+              }
+              alt={`배너이미지}`}
+              style={{
+                width: "100%",
+              }}
+            />
+          </Flex>
+        ))}
+      </Slider>
+    </SliderWrapper>
   );
 };
 export default Banner;
+const SliderWrapper = styled.div`
+  position: relative;
+  width: 1000px;
+  margin: 0 auto;
+  .slick-list {
+    width: 100%;
+  }
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+  .slick-dots {
+    bottom: -15px;
+  }
+  .slick-dots li {
+    margin: 0;
+  }
+  .slick-dots li button {
+    padding: 0;
+    &:before {
+      color: #d89999;
+      opacity: 0.5;
+    }
+  }
+  .slick-dots li.slick-active button:before {
+    color: #333;
+    opacity: 0.7;
+  }
+`;
