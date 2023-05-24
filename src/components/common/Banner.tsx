9ebc3a4393f224c388bc7614f8de9fd343d49b4c
hajungkim <hajung3406@naver.com>
2023-05-24 09:53:06 +0900

    배너 이미지 적용

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -5,8 +5,25 @@ import "slick-carousel/slick/slick-theme.css";
 import { Flex } from "@chakra-ui/react";
 const Banner = () => {
   return (
-    <Flex p="0 30px">
-      {[1, 2].map((el) => (
+    <Flex p="0 30px" m="0 auto">
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="50px"
+        w="100%"
+      >
+        <img
+          src={
+            process.env.PUBLIC_URL + `/assets/image/banner/banner-main-1.png`
+          }
+          alt={`배너이미지}`}
+          style={{
+            width: "100%",
+            borderRadius: "20px",
+          }}
+        />
+      </Flex>
+      {/* {[1, 2].map((el) => (
         <Flex key={el} justifyContent="center" alignItems="center">
           <img
             src={
@@ -15,7 +32,7 @@ const Banner = () => {
             alt={`배너이미지-${el}}`}
           />
         </Flex>
-      ))}
+      ))} */}
     </Flex>
   );
 };
