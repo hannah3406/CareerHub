bf52edb69719e0738efafdece575605b71cefc30
hajungkim <hajung3406@naver.com>
2023-05-25 10:01:45 +0900

    쿠키 withCredentials 설정

--- src/apis/_axios/instance.ts
+++ src/apis/_axios/instance.ts
@@ -8,6 +8,7 @@ const instance: AxiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 const setAuthHeader = (token: string) => {
